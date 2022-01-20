284fd753b554ed0d5aa9cd2e5d4f92cd96c62a49
Syba789 <95074177+Syba789@users.noreply.github.com>
2022-01-20 09:11:09 +0000

    done

---
+++ p6final.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int input()
+{
+  int x;
+  scanf("%d",&x);
+  return x;
+}
+int sum_n(int n)
+{
+  int r=0;
+  r=(n*(n+1))/2;
+  return r;
+}
+void output(int n,int sum)
+{
+  printf("1");
+  for(int f=2;f<n;f=f+1)
+  {
+    printf("+%d is %d\n",n,sum);
+  }
+}
+int main()
+{
+  int n,sum;
+  n=input();
+  sum=sum_n(n);
+  output(n,sum);
+  return 0;
+}
